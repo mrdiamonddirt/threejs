7e1a96743180906dc16716d4703d680d54b5729c
RowDog <mrdiamonddirt@gmail.com>
2023-02-03 21:07:54 +0000

    add a simple object

--- main.js
+++ main.js
@@ -21,3 +21,21 @@ camera.position.setZ(30);
 
 // finally render the scene
 renderer.render(scene, camera);
+
+// create the geomantry
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+// create the material
+const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+// create the mesh adding the geometry and the material
+const torus = new THREE.Mesh(geometry, material);
+
+// add the mesh to the scene
+scene.add(torus);
+
+// create a function to animate the scene
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
