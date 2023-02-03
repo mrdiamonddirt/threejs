1220d62f3e6c144b3ef6612913d9d5f637af5114
RowDog <mrdiamonddirt@gmail.com>
2023-02-03 21:04:31 +0000

    create a scence in threejs

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 import './style.css'
+
+import * as THREE from 'three';
+
+// create a scene
+const scene = new THREE.Scene();
+
+// create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// create a renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+// set the size of the renderer
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+// set the position of the camera
+camera.position.setZ(30);
+
+// finally render the scene
+renderer.render(scene, camera);
